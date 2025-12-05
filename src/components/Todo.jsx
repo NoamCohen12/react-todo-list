63cf48ad76bee68e72dc17ef277fe40b1587249e
NoamCohen12 <noam12345002@gmail.com>
2025-12-05 11:46:38 +0200

    Add Todo component: - Created input and button for adding todos - Styled the component - Implemented basic logic for handling input and button click

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,22 @@
 import './Todo.css'
-
+import { useState } from 'react'
 function Todo() {
+    const [todo, setTodo] = useState('')
+
+    function oneClickAdd() {
+        if (todo !== '') {
+            console.log("oneClickAdd", todo)
+        }
+        setTodo('')
+    }
+    function oneClickChange(event) {
+        setTodo(event.target.value)
+    }
+
     return (
         <div className="todo">
-            Todo
+            <input type="text" value={todo} onChange={oneClickChange} placeholder="Create a new todo" />
+            <button onClick={oneClickAdd}>Add</button>
         </div>
     )
 }
